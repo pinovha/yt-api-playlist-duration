ef99f5520de0574dd03fa952e48361d61eca65e0
pinovha <kajfunretro12@gmail.com>
2025-05-09 17:10:44 +0200

    Fetch API_KEY from environment and build Youtube API client

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from googleapiclient.discovery import build
 from datetime import timedelta
+from isodate import parse_duration
+import os
+
+# In the terminal, set the environment variable:
+# export API_KEY="your-api-key-here"
+
+# Fetch the API_KEY from the environment
+API_KEY = os.getenv("API_KEY")
+
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
+PLAYLIST_ID = ""
+
+youtube = build(API_SERVICE_NAME, API_VERSION, developerKey='API_KEY')
+

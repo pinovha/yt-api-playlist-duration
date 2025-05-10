bb51526cbaf291120c2c604ec5fe617875b1469b
pinovha <kajfunretro12@gmail.com>
2025-05-10 14:44:58 +0200

    Optimize API calls to reduce number of requests

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ total_duration = timedelta()
 while True:
     try:
         response = youtube.playlistItems().list(
-            # Ask for 'snippet' to get video metadata
-            part="snippet",
+            # Ask for 'contentDetails' to get video metadata
+            part="contentDetails",
             playlistId=PLAYLIST_ID,
             # maxResults cannot exceed 50 (API limit)
             maxResults=50,
@@ -47,34 +47,38 @@ while True:
             pageToken=next_page_token
         ).execute()
 
+        # Initialize a list to store video IDs 
+        videos_ids = []
+        
         # Iterate through each video in the playlist response
         for item in response['items']:
-            # Extract the title of the video
-            title = item['snippet']['title']
-            # Extract video ID from the playlist item
-            video_id = item['snippet']['resourceId']['videoId']
-    
-            try:
-                # Fetch video details using video ID
-                video_response = youtube.videos().list(
-                    # Ask for details of the video
-                    part="contentDetails",
-                    # Identyify the video by its ID
-                    id=video_id
-                ).execute()
+            # Extract video ID from the playlist item and append to the list
+            videos_ids.append(item['contentDetails']['videoId'])
+            
+        try:
+            # Fetch video details using video ID
+            video_response = youtube.videos().list(
+                # Ask for details of the video
+                part="contentDetails",
+                # Combine all collected 50 video IDs into a single string, separated by commas
+                id=','.join(videos_ids)
+            ).execute()
 
-                # Extract the duration data from the video_response, which is in 'ISO 8601 duration format'
-                duration_iso = video_response['items'][0]['contentDetails']['duration']
-                # Convert ISO 8601 duration string intro a Python timedelta object
-                duration = parse_duration(duration_iso)
-                # Add the duration of current video to the total_duration
-                total_duration += duration
+        # Display error message and continue processing
+        except HttpError as error:
+            print(f"\nFailed to fetch video details. \n\nError: {error}\n")
+            continue
 
-            # Display error message and continue processing
-            except HttpError as error:
-                print(f"\nFailed to fetch video details. Video ID: {video_id} \n\nError: {error}\n")
-                continue
+        # Iterate through each item in video_response
+        for item in video_response['items']:
+            # Extract the duration data from the video_response, which is in 'ISO 8601 duration format'
+            duration_iso = item['contentDetails']['duration']
 
+            # Convert ISO 8601 duration string intro a Python timedelta object
+            duration = parse_duration(duration_iso)
+
+            # Add the duration of current video to the total_duration
+            total_duration += duration
 
         # Get nextPageToken from response to fetch the next page of results
         next_page_token = response.get('nextPageToken')
